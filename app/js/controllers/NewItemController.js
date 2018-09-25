5871717610266c0e45e6e85ad04b4859d9ca6cac
robert-chenault-tm <robert.chenault.jobs@gmail.com>
2018-09-25 17:42:50 -0400

    Items
    Added the ability to create armor

--- app/js/controllers/NewItemController.js
+++ app/js/controllers/NewItemController.js
@@ -1,13 +1,17 @@
 dndApp.controller('NewItemController', function($scope, $location, ItemService) {
 	$scope.item = {};
 	$scope.sendingToDB = false;
+	$scope.isArmor = false;
+	$scope.isWeapon = false;
 	$scope.item.weaponMods = null;
 	$scope.item.armorMods = null;
+	$scope.armorTypes = ['Light', 'Medium', 'Heavy', 'Shield'];
 	
 	$scope.createItem = function(itemForm) {
 		if(itemForm.$valid) {
 			$scope.sendingToDB = true;
 			$scope.item.username = $scope.globals.currentUser.username;
+			console.log($scope.item);
 			ItemService.createItem($scope.item, function(response) {
 				console.log(response);
 				$location.url("/items");
@@ -20,5 +24,21 @@ dndApp.controller('NewItemController', function($scope, $location, ItemService) 
 	
 	$scope.cancelCreate = function() {
 		$location.url("/items");
+	}
+	
+	$scope.toggleWeaponSection = function() {
+		
+	}
+	
+	$scope.toggleArmorSection = function() {
+		if($scope.isArmor) {
+			console.log("true");
+			$scope.item.armorMods = {};
+			$scope.item.armorMods.armorType = $scope.armorTypes[0];
+			$scope.item.armorMods.stealthDisadvantage = false;
+		} else {
+			console.log("false");
+			$scope.item.armorMods = null;
+		}
 	}
 });
